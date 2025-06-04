6d05c9099123fe35dc3ac3504878a136773bef35
woodid012 <dbwooding88@gmail.com>
2025-06-04 16:01:05 +1000

    css

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-        os: false,
-      };
-    }
-    return config;
+const config = {
+  plugins: {
+    "@tailwindcss/postcss": {},
   },
 };
-
-module.exports = nextConfig;
+export default config;
